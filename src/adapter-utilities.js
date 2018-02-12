4d94f11b15257c177d49e2f9df5b9dff9a78efc9
Hendrik Volschenk <righteous.trespasser@gmail.com>
2018-02-12 23:34:37 +0200

    fix: material 030 upgrade. move adapter utilities singleton to instance

--- src/adapter-utilities.js
+++ src/adapter-utilities.js
@@ -1,46 +1,41 @@
 const PASSIVE_EVENT_LISTENERS = ['touchstart'];
-let classNames = [];
 
-export const addClass = updateClassNames => (className) => {
-  classNames = [...classNames, className];
-  updateClassNames(classNames);
+export default () => {
+  let classNames = [];
+
+  const deregisterInteractionHandler = (element, type) => handler =>
+    element.removeEventListener(type, handler);
+  const registerInteractionHandler = (element, type) => handler =>
+    element.addEventListener(
+      type,
+      handler,
+      PASSIVE_EVENT_LISTENERS.includes(type) ? { passive: true } : null,
+    );
+
+  return {
+    addClass: updateClassNames => (className) => {
+      classNames = [...classNames, className];
+      updateClassNames(classNames);
+    },
+    deregisterInteractionHandler,
+    deregisterInteractionHandlerAny: element => (type, handler) =>
+      deregisterInteractionHandler(element, type)(handler),
+    registerInteractionHandler,
+    registerInteractionHandlerAny: element => (type, handler) =>
+      registerInteractionHandler(element, type)(handler),
+    removeClass: updateClassNames => (className) => {
+      classNames = classNames.filter(currentClassName => currentClassName !== className);
+      updateClassNames(classNames);
+    },
+    setActionAriaHidden: updateActionButtonAriaHidden => () =>
+      updateActionButtonAriaHidden('true'),
+    setActionText: updateActionButtonText => text => updateActionButtonText(text),
+    setAriaHidden: updateSnackbarAriaHidden => () => updateSnackbarAriaHidden('true'),
+    setFocus: elementActionButton => () => elementActionButton.focus(),
+    setMessageText: updateSnackbarText => text => updateSnackbarText(text),
+    unsetActionAriaHidden: updateActionButtonAriaHidden => () =>
+      updateActionButtonAriaHidden('false'),
+    unsetAriaHidden: updateSnackbarAriaHidden => () => updateSnackbarAriaHidden('false'),
+    visibilityIsHidden: () => () => document.hidden,
+  };
 };
-
-export const deregisterInteractionHandler = (element, type) => handler =>
-  element.removeEventListener(type, handler);
-
-export const deregisterInteractionHandlerAny = element => (type, handler) =>
-  deregisterInteractionHandler(element, type)(handler);
-
-export const registerInteractionHandler = (element, type) => handler =>
-  element.addEventListener(
-    type,
-    handler,
-    PASSIVE_EVENT_LISTENERS.includes(type) ? { passive: true } : null,
-  );
-
-export const registerInteractionHandlerAny = element => (type, handler) =>
-  registerInteractionHandler(element, type)(handler);
-
-export const removeClass = updateClassNames => (className) => {
-  classNames = classNames.filter(currentClassName => currentClassName !== className);
-  updateClassNames(classNames);
-};
-
-export const setActionAriaHidden = updateActionButtonAriaHidden => () =>
-  updateActionButtonAriaHidden('true');
-
-export const setActionText = updateActionButtonText => text => updateActionButtonText(text);
-
-export const setAriaHidden = updateSnackbarAriaHidden => () => updateSnackbarAriaHidden('true');
-
-export const setFocus = elementActionButton => () => elementActionButton.focus();
-
-export const setMessageText = updateSnackbarText => text => updateSnackbarText(text);
-
-export const unsetActionAriaHidden = updateActionButtonAriaHidden => () =>
-  updateActionButtonAriaHidden('false');
-
-export const unsetAriaHidden = updateSnackbarAriaHidden => () => updateSnackbarAriaHidden('false');
-
-export const visibilityIsHidden = () => () => document.hidden;
